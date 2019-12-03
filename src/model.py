9d0f660f5768878e963eea2c699048f490c0c675
Marcel <MarcelFrueh@gmx.de>
2019-12-03 21:33:34 +0100

    Encoder

--- src/model.py
+++ src/model.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 from partialconv2d import PartialConv2d
 
+
 class DeFINe(nn.Module):
     """
     This class implements the Deep Facial Inpainting Model
@@ -16,34 +17,49 @@ class DeFINe(nn.Module):
 
         # Now we define the model
         # The model is a Unet, but uses the partial convolutions from nvidia
-        self.pconv1 = PartialConv2d(3, 64, kernel_size=7, stride=2, multi_channel=True, return_mask=True)
-        self.pconv2 = PartialConv2d(64, 128, kernel_size=5, stride=2, multi_channel=True, return_mask=True)
+        self.encode = [
+            PartialConv2d(3, 64, kernel_size=7, stride=2, multi_channel=True, return_mask=True),
+            PartialConv2d(64, 128, kernel_size=5, stride=2, multi_channel=True, return_mask=True),
 
-        self.pconv3 = PartialConv2d(128, 256, kernel_size=5, stride=2, multi_channel=True, return_mask=True)
-        self.pconv4 = PartialConv2d(256, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True)
-        self.pconv5 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True)
-        self.pconv6 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True)
-        self.pconv7 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True)
-        self.pconv8 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True)
+            PartialConv2d(128, 256, kernel_size=5, stride=2, multi_channel=True, return_mask=True),
+            PartialConv2d(256, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True, return_mask=True),
 
-        self.pconv9 = PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv10 = PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv11 = PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv12 = PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv13 = PartialConv2d(512, 256, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv14 = PartialConv2d(256, 128, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv15 = PartialConv2d(128, 64, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
-        self.pconv16 = PartialConv2d(64, 3, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
+            PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 512, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(512, 256, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(256, 128, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(128, 64, kernel_size=3, stride=1, multi_channel=True, return_mask=True),
+            PartialConv2d(64, 3, kernel_size=3, stride=1, multi_channel=True, return_mask=True)
+        ]
 
     def forward(self, img):
+        """
+        Inference on image
+        :param img: batch of numpy arrays [b, 3, h, w]
+        :return: tensor with inpainted image [b, 3, h, w]
+        """
         img = torch.tensor(img, dtype=torch.float)
-        out1, mask1 = self.forward_block(self.pconv1, img, False)
-        out2, mask2 = self.forward_block(self.pconv2, out1, True)
+        out1, mask1 = self.forward_block(self.encode[0], img, False)
+        out2, mask2 = self.forward_block(self.encode[1], out1, True)
 
         return out1
 
-    def forward_block(self, operation, tensor, bn=True):
+    def forward_block(self, operation, tensor, use_batchnorm=True):
+        """
+        Runs ReLU and BatchNorm on Conv Output
+        :param operation: Convolution Operation
+        :param tensor: Image Output from last block
+        :param use_batchnorm: Use BatchNorm?
+        :return:
+        """
+
         out, mask = operation(tensor)
-        if bn:
+        if use_batchnorm:
             out = nn.BatchNorm2d(operation.out_channels)(out)
         return nn.ReLU()(out), mask
