0f03e795bdbb0d018c61e8e9f2cd7759f1dfcd69
Marcel <MarcelFrueh@gmx.de>
2019-12-11 12:27:24 +0100

    Command Line Arguments

--- src/train.py
+++ src/train.py
@@ -2,6 +2,8 @@
 Train the model
 """
 
+import argparse
+import os
 from torch.utils import data
 from torch.utils.tensorboard import SummaryWriter
 import torch
@@ -10,12 +12,24 @@ import loss
 from model import DeFINe
 from loader import Dataset
 
+parser = argparse.ArgumentParser(description="Path to face images and masks")
+parser.add_argument('--img_path', help='path to image folder (up to images512x512)',
+                    default='../dat/Faces/ffhq-dataset/images512x512/')
+parser.add_argument('--mask_path', help='path to image folder (up to train or test)',
+                    default='../dat/qd_imd/train/')
+parser.add_argument('--checkpoint_path', help='path to store the checkpoints',
+                    default='../dat/qd_imd/train/')
+
+args = parser.parse_args()
+faces_path = args.img_path
+mask_path = args.mask_path
+checkpoint_path = args.checkpoint_path
+
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 
 NET = DeFINe(device=device)
 NET.to(device)
-
 
 params = {'batch_size': 4,
           'shuffle': False,
@@ -23,7 +37,7 @@ params = {'batch_size': 4,
 
 max_epochs = 2000
 
-training_set = Dataset()
+training_set = Dataset(faces_path=faces_path, mask_path=mask_path)
 training_generator = data.DataLoader(training_set, **params)
 
 validation_set = Dataset(mask_path='../dat/qd_imd/test/')
@@ -51,6 +65,5 @@ for epoch in range(max_epochs):
         grids = create_grids(masked_img, pred)
         write_to_tensorboard(writer, grids, actual_loss, GLOBAL_STEP)
         GLOBAL_STEP += 1
-
-    model_path = f'../ckt/{epoch}'
+    model_path = os.path.join(checkpoint_path, str(epoch))
     torch.save(NET.state_dict(), model_path)
