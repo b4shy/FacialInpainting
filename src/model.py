1ce534f81ba104792b1d25eef5c91e1d9b3fb8af
Marcel <MarcelFrueh@gmx.de>
2019-12-18 10:09:03 +0100

    Batch Norm

--- src/model.py
+++ src/model.py
@@ -26,59 +26,73 @@ class DeFINe(nn.Module):
         self.encode1 = PartialConv2d(64, 128, kernel_size=5, stride=2, multi_channel=True,
                                      return_mask=True, padding=2)
         kaiming_normal_(self.encode1.weight, nonlinearity='relu')
+        self.batchnorm1 = nn.BatchNorm2d(128)
 
         self.encode2 = PartialConv2d(128, 256, kernel_size=5, stride=2, multi_channel=True,
                                      return_mask=True, padding=2)
         kaiming_normal_(self.encode2.weight, nonlinearity='relu')
+        self.batchnorm2 = nn.BatchNorm2d(256)
 
         self.encode3 = PartialConv2d(256, 512, kernel_size=3, stride=2, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.encode3.weight, nonlinearity='relu')
+        self.batchnorm3 = nn.BatchNorm2d(512)
 
         self.encode4 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.encode4.weight, nonlinearity='relu')
+        self.batchnorm4 = nn.BatchNorm2d(512)
 
         self.encode5 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.encode5.weight, nonlinearity='relu')
+        self.batchnorm5 = nn.BatchNorm2d(512)
 
         self.encode6 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.encode6.weight, nonlinearity='relu')
+        self.batchnorm6 = nn.BatchNorm2d(512)
 
         self.encode7 = PartialConv2d(512, 512, kernel_size=3, stride=2, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.encode7.weight, nonlinearity='relu')
+        self.batchnorm7 = nn.BatchNorm2d(512)
 
         # Decoding Layers
         self.decode0 = PartialConv2d(1024, 512, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode0.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm8 = nn.BatchNorm2d(512)
 
         self.decode1 = PartialConv2d(1024, 512, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode1.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm9 = nn.BatchNorm2d(512)
 
         self.decode2 = PartialConv2d(1024, 512, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode2.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm10 = nn.BatchNorm2d(512)
 
         self.decode3 = PartialConv2d(1024, 512, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode3.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm11 = nn.BatchNorm2d(512)
 
         self.decode4 = PartialConv2d(768, 256, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode4.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm12 = nn.BatchNorm2d(256)
 
         self.decode5 = PartialConv2d(384, 128, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode5.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm13 = nn.BatchNorm2d(128)
 
         self.decode6 = PartialConv2d(192, 64, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
         kaiming_normal_(self.decode6.weight, a=0.2, nonlinearity='leaky_relu')
+        self.batchnorm14 = nn.BatchNorm2d(64)
 
         self.decode7 = PartialConv2d(67, 3, kernel_size=3, stride=1, multi_channel=True,
                                      return_mask=True, padding=1)
@@ -94,27 +108,34 @@ class DeFINe(nn.Module):
         img = masked_image.permute(0, 3, 1, 2)
         mask = input_mask.permute(0, 3, 1, 2)
 
-        out0, mask0 = self.forward_encode_block(self.encode0, img, mask, False)
-        out1, mask1 = self.forward_encode_block(self.encode1, out0, mask0, False)
-        out2, mask2 = self.forward_encode_block(self.encode2, out1, mask1, False)
-        out3, mask3 = self.forward_encode_block(self.encode3, out2, mask2, False)
-        out4, mask4 = self.forward_encode_block(self.encode4, out3, mask3, False)
-        out5, mask5 = self.forward_encode_block(self.encode5, out4, mask4, False)
-        out6, mask6 = self.forward_encode_block(self.encode6, out5, mask5, False)
-        out7, mask7 = self.forward_encode_block(self.encode7, out6, mask6, False)
+        out0, mask0 = self.forward_encode_block(self.encode0, img, mask, None)
+        out1, mask1 = self.forward_encode_block(self.encode1, out0, mask0, self.batchnorm1)
+        out2, mask2 = self.forward_encode_block(self.encode2, out1, mask1, self.batchnorm2)
+        out3, mask3 = self.forward_encode_block(self.encode3, out2, mask2, self.batchnorm3)
+        out4, mask4 = self.forward_encode_block(self.encode4, out3, mask3, self.batchnorm4)
+        out5, mask5 = self.forward_encode_block(self.encode5, out4, mask4, self.batchnorm5)
+        out6, mask6 = self.forward_encode_block(self.encode6, out5, mask5, self.batchnorm6)
+        out7, mask7 = self.forward_encode_block(self.encode7, out6, mask6, self.batchnorm7)
 
-        decode_out0, decode_mask0 = self.forward_decode_block(self.decode0, out7, mask7, out6, mask6)
-        decode_out1, decode_mask1 = self.forward_decode_block(self.decode1, decode_out0, decode_mask0, out5, mask5)
-        decode_out2, decode_mask2 = self.forward_decode_block(self.decode2, decode_out1, decode_mask1, out4, mask4)
-        decode_out3, decode_mask3 = self.forward_decode_block(self.decode3, decode_out2, decode_mask2, out3, mask3)
-        decode_out4, decode_mask4 = self.forward_decode_block(self.decode4, decode_out3, decode_mask3, out2, mask2)
-        decode_out5, decode_mask5 = self.forward_decode_block(self.decode5, decode_out4, decode_mask4, out1, mask1)
-        decode_out6, decode_mask6 = self.forward_decode_block(self.decode6, decode_out5, decode_mask5, out0, mask0)
-        img_out, mask_out = self.forward_decode_block(self.decode7, decode_out6, decode_mask6, img, mask)
+        decode_out0, decode_mask0 = self.forward_decode_block(self.decode0, out7, mask7,
+                                                              out6, mask6, self.batchnorm8)
+        decode_out1, decode_mask1 = self.forward_decode_block(self.decode1, decode_out0, decode_mask0,
+                                                              out5, mask5, self.batchnorm9)
+        decode_out2, decode_mask2 = self.forward_decode_block(self.decode2, decode_out1, decode_mask1,
+                                                              out4, mask4, self.batchnorm10)
+        decode_out3, decode_mask3 = self.forward_decode_block(self.decode3, decode_out2, decode_mask2,
+                                                              out3, mask3, self.batchnorm11)
+        decode_out4, decode_mask4 = self.forward_decode_block(self.decode4, decode_out3, decode_mask3,
+                                                              out2, mask2, self.batchnorm12)
+        decode_out5, decode_mask5 = self.forward_decode_block(self.decode5, decode_out4, decode_mask4,
+                                                              out1, mask1, self.batchnorm13)
+        decode_out6, decode_mask6 = self.forward_decode_block(self.decode6, decode_out5, decode_mask5,
+                                                              out0, mask0, self.batchnorm14)
+        img_out, mask_out = self.forward_decode_block(self.decode7, decode_out6, decode_mask6, img, mask, None)
 
         return img_out
 
-    def forward_encode_block(self, operation, img_tensor, mask_tensor, use_batchnorm=False):
+    def forward_encode_block(self, operation, img_tensor, mask_tensor, batch_norm_op):
         """
         Runs one operation from the encode block
         Runs ReLU and (maybe)BatchNorm on Conv Output
@@ -126,12 +147,12 @@ class DeFINe(nn.Module):
         """
 
         out, mask = operation(img_tensor, mask_tensor)
-        if use_batchnorm:
-            out = nn.BatchNorm2d(operation.out_channels)(out)
+        if batch_norm_op:
+            out = batch_norm_op(out)
         return nn.ReLU()(out), mask
 
     def forward_decode_block(self, operation, img_tensor, mask_tensor, concat_with_img, concat_with_mask,
-                             use_batchnorm=False):
+                             batch_norm_op):
         """
         Runs one operation from the decode block
         First Upsamples with
@@ -151,7 +172,7 @@ class DeFINe(nn.Module):
         concat_img_tensor = torch.cat((upsampled_img_tensor, concat_with_img), dim=1)
         concat_mask_tensor = torch.cat((upsampled_mask_tensor, concat_with_mask), dim=1)
         out, mask = operation(concat_img_tensor, concat_mask_tensor)
-        if use_batchnorm:
-            out = nn.BatchNorm2d(operation.out_channels)(out)
+        if batch_norm_op:
+            out = batch_norm_op(out)
         return nn.LeakyReLU(0.2)(out), mask
 
