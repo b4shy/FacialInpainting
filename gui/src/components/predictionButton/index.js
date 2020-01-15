f5e079c6338450e54a76d964fbf3369aa933ddfe
Micha Schilling <micha@die-schillings.de>
2020-01-15 22:51:25 +0100

    react app now sends json array

--- gui/src/components/predictionButton/index.js
+++ gui/src/components/predictionButton/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { IMAGE_SIZE } from '../../constants'
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
 
 
 export default class index extends Component {
@@ -9,20 +13,22 @@ export default class index extends Component {
         super(props);
         this.state = {
             isLoading: false,
+            dialogOpen: false
         };
+        this.handleClose = this.handleClose.bind(this);
     }
     async predict() {
         const editCanvas = this.props.editCanvas.current.canvasRef.current;
         const editContext = editCanvas.getContext('2d');
         const editData = editContext.getImageData(0, 0, IMAGE_SIZE.width, IMAGE_SIZE.height).data;
-        const maskPNG = editCanvas.toDataURL("image/png");
+        //const maskPNG = editCanvas.toDataURL("image/png");
         //console.log("predict:", this.props.editCanvas.current.canvasRef.current.toDataURL());
         const imageCanvas = this.props.imageCanvas.current.canvasRef.current;
         const imageContext = imageCanvas.getContext('2d');
         const imageData = imageContext.getImageData(0, 0, IMAGE_SIZE.width, IMAGE_SIZE.height).data;
-        const imgPNG = imageCanvas.toDataURL("image/png");
+        //const imgPNG = imageCanvas.toDataURL("image/png");
 
-        /*var newImage = new Array(IMAGE_SIZE.height);
+        var newImage = new Array(IMAGE_SIZE.height);
         for (var y = 0; y < IMAGE_SIZE.height; y++) {
             var imageRow = new Array(IMAGE_SIZE.width);
             for (var x = 0; x < IMAGE_SIZE.width; x++) {
@@ -36,11 +42,11 @@ export default class index extends Component {
                 imageRow[x] = pixel;
             }
             newImage[y] = imageRow;
-        }*/
+        }
 
 
         //console.log("predict:", newImage);
-
+        /*
         var fileName = "networkInputImage";
         //const json = JSON.stringify(newImage);
         //const blob = new Blob([json], { type: 'application/json' });
@@ -61,27 +67,66 @@ export default class index extends Component {
         link.download = fileName + ".png";
         document.body.appendChild(link);
         link.click();
-        document.body.removeChild(link);
+        document.body.removeChild(link);*/
 
         //TODO
-        /*var xhr = new XMLHttpRequest()
-        xhr.addEventListener('load', () => {
-          console.log(xhr.responseText)
-        })
-        xhr.open('GET', 'https://dog.ceo/api/breeds/list/all')
-        xhr.send()*/
+        this.setState({ isLoading: true });
+        fetch('/inference', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                image: newImage,
+            })
+        }).then(response => {
+            this.setState({ isLoading: false });
+            //console.log(response)
+            this.setState({ dialogOpen: true });
+
+            var data = JSON.parse(response);
+            var image = new Image();
+            image.onload = function () {
+                const canvas = this.canvasRef.current;
+                const ctx = canvas.getContext('2d');
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(image, 0, 0); // draw the new image to the screen
+            }
+            image.src = data.image;
+        });
     }
+
+    handleClose() {
+        //console.log("this.state.dialogOpen");
+        this.setState({ dialogOpen: false });
+    };
 
     render() {
         return (
-            <div style={{position: 'relative'}}>
+            <div style={{ position: 'relative' }}>
                 <Button variant="outlined" color="primary" onClick={this.predict.bind(this)} disabled={this.state.isLoading}>
                     Predict
-                {this.state.isLoading && <CircularProgress size={24} style={{position: 'absolute', top: '50%',
-      left: '50%',
-      marginTop: -12,
-      marginLeft: -12}}/>}
+                {this.state.isLoading && <CircularProgress size={24} style={{
+                        position: 'absolute', top: '50%',
+                        left: '50%',
+                        marginTop: -12,
+                        marginLeft: -12
+                    }} />}
                 </Button>
+                <Dialog onClose={this.handleClose} aria-labelledby="dialog-title" open={this.state.dialogOpen}>
+                    <DialogTitle id="dialog-title" onClose={this.handleClose}>
+                        Load Image
+                    </DialogTitle>
+                    <DialogContent dividers>
+                        <canvas ref={this.canvasRef} height={IMAGE_SIZE.height} width={IMAGE_SIZE.width}></canvas>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Close
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         )
     }
